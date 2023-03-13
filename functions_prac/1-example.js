4ae18f61ff95c04ee6db6ade1e043216ed3069bc
waweroh <scadmsez@gmail.com>
2023-03-13 13:18:23 +0300

    learning js

---
+++ functions_prac/1-example.js
@@ -0,0 +1,12 @@
+/*function function name (parameters){
+    func body-defines what func does;
+    loops/conditions(optional)
+    return VALUE 
+}
+*/
+function addNumber(x, y, z){
+    let sum = x + y + z;
+    return sum;
+}
+let answer = addNumber(3, 10, 34);
+console.log(answer);
